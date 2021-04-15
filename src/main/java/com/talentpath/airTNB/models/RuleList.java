90b38e0cc6cf5906289a2ad878d9b6b37219685f
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-04-15 14:28:46 -0500

    added parameters to RuleList model cooresponding to each rule. also created an enum to use with it.

--- src/main/java/com/talentpath/airTNB/models/RuleList.java
+++ src/main/java/com/talentpath/airTNB/models/RuleList.java
@@ -8,6 +8,16 @@ public class RuleList {
     @Id
     private Integer id;
 
+    private String checkInTime;
+    private String checkOutTime;
+    private Boolean smokingAllowed;
+    private Boolean petsAllowed;
+    private Boolean childSuitable;
+    private Boolean partiesAllowed;
+    private CheckInMethod checkInMethod;
+
+
+
     @OneToOne(fetch= FetchType.LAZY)
     @MapsId
     private ThingToKnow thingToKnow;
