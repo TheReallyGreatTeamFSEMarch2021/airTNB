fcb38a078c31d1366085a3beca3414c5e6a50147
Jesse-Lavendt <jesse@opal.dev>
2021-04-19 16:24:04 -0400

    added files to git

--- src/main/java/com/talentpath/airTNB/controllers/AmenityController.java
+++ src/main/java/com/talentpath/airTNB/controllers/AmenityController.java
@@ -1,2 +1,24 @@
-package com.talentpath.airTNB.controllers;public class AmenityController {
+package com.talentpath.airTNB.controllers;
+
+import com.talentpath.airTNB.models.Amenity;
+import com.talentpath.airTNB.services.AmenityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/amenity")
+public class AmenityController {
+
+    @Autowired
+    AmenityService amenityServ;
+
+    @GetMapping("/getByListingId/{listingId}")
+    public List<Amenity> getInfosByItemId(@PathVariable Integer listingId) {
+
+        return amenityServ.getAllByListing(listingId);
+    }
+
 }
--- src/main/java/com/talentpath/airTNB/models/Location.java
+++ src/main/java/com/talentpath/airTNB/models/Location.java
@@ -13,7 +13,7 @@ public class Location {
 
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
     @OneToMany(mappedBy="location", cascade=CascadeType.ALL)
