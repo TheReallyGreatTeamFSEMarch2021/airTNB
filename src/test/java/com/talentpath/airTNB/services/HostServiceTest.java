f2a36107f03834cf48b0747f8ea28cd6c4486917
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-04-21 16:56:34 -0500

    added unit tests for service layer for host

---
+++ src/test/java/com/talentpath/airTNB/services/HostServiceTest.java
@@ -0,0 +1,58 @@
+package com.talentpath.airTNB.services;
+
+import com.talentpath.airTNB.daos.HostRepository;
+import com.talentpath.airTNB.models.Host;
+import com.talentpath.airTNB.models.Listing;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class HostServiceTest {
+
+    @Autowired
+    HostService hostService;
+    @Autowired
+    HostRepository hostRepository;
+
+    @BeforeEach
+    @Transactional
+    void setUp() {
+        hostRepository.deleteAll();
+        Host host = new Host();
+        host.setSuperHost(true);
+        host.setDescription("a host");
+        host.setVerified(true);
+        Listing listing = new Listing();
+        listing.setTitle("a listing");
+        listing.setCity("Houston");
+        host.setListings(List.of(listing));
+        hostRepository.saveAndFlush(host);
+    }
+
+    @Test
+    void getAllHosts() {
+        List<Host> list = hostService.getAllHosts();
+        assertEquals(1,list.get(0).getId());
+    }
+
+    @Test
+    void getHostById() {
+    }
+
+    @Test
+    void addHost() {
+    }
+}
