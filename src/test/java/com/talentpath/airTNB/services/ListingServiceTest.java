81b4cebcfe43345d8c20e4231402a19eaa72a324
Fei Yang <fyang@C02CMKHKML7H.local>
2021-04-23 15:09:36 -0400

    Added JUnit tests for LocationService

--- src/test/java/com/talentpath/airTNB/services/ListingServiceTest.java
+++ src/test/java/com/talentpath/airTNB/services/ListingServiceTest.java
@@ -1,6 +1,10 @@
 package com.talentpath.airTNB.services;
 
+import com.talentpath.airTNB.daos.AmenityRepository;
+import com.talentpath.airTNB.daos.ListingRepository;
+import com.talentpath.airTNB.daos.LocationRepository;
 import com.talentpath.airTNB.models.Listing;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +14,22 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-
-
 class ListingServiceTest {
 
     @Autowired
-    ListingService listingServ;
+    ListingService listingServ; //the service that we are testing
+
+    @Autowired
+    ListingRepository listingRepo;
+    @Autowired
+    AmenityRepository amenityRepo;
+
+
+    @BeforeEach
+    void deleteAllItems(){
+        amenityRepo.deleteAll();
+        listingRepo.deleteAll();
+    }
 
     @Test
     void getAllListings() {
@@ -26,11 +40,7 @@ class ListingServiceTest {
     }
 
     @Test
-    void addListing() {
-        Listing listing = new Listing();
-        listing.setTitle("house");
-        listingServ.addListing(listing);
+    void test(){
     }
-
 
 }
