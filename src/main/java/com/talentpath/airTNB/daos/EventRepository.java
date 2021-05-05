6ed95c4d789ffc983ed9a3f49e58e5a9f7372064
jlavendtt <68912686+jlavendtt@users.noreply.github.com>
2021-05-05 14:52:21 -0400

    added events model and controller (#33)
    Co-authored-by: Jesse-Lavendt <jesse@opal.dev>

---
+++ src/main/java/com/talentpath/airTNB/daos/EventRepository.java
@@ -0,0 +1,13 @@
+package com.talentpath.airTNB.daos;
+
+import com.talentpath.airTNB.models.Amenity;
+import com.talentpath.airTNB.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Integer> {
+    List<Event> findBylistingId(Integer id);
+}
